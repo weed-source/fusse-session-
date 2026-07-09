3984fe27dbef38c28898f45408c0c0064281ca0d
weed-source <dasoodeborah@gmail.com>
2026-07-09 09:25:19 -0400

    Export makeid function for module usage

--- gen-id.js
+++ gen-id.js
@@ -1,4 +1,4 @@
-function makeid(num = 4) {
+export function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
@@ -7,4 +7,3 @@ function makeid(num = 4) {
   }
   return result;
 }
-module.exports = {makeid};
